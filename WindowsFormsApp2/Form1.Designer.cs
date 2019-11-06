18b2edeb5220906f0b36b92422e9adae7ad3ffb4
sakal <tumsakal.ts@gmail.com>
2019-11-06 09:59:24 +0700

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TestSqlCommon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.rdoMsAccess = new System.Windows.Forms.RadioButton();
+            this.rdoSqlServer = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 331);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(12, 78);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(185, 23);
+            this.btnLoadData.TabIndex = 1;
+            this.btnLoadData.Text = "Load Data";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.BtnLoadData_Click);
+            // 
+            // rdoMsAccess
+            // 
+            this.rdoMsAccess.AutoSize = true;
+            this.rdoMsAccess.Location = new System.Drawing.Point(12, 55);
+            this.rdoMsAccess.Name = "rdoMsAccess";
+            this.rdoMsAccess.Size = new System.Drawing.Size(77, 17);
+            this.rdoMsAccess.TabIndex = 2;
+            this.rdoMsAccess.TabStop = true;
+            this.rdoMsAccess.Text = "Ms Access";
+            this.rdoMsAccess.UseVisualStyleBackColor = true;
+            this.rdoMsAccess.CheckedChanged += new System.EventHandler(this.RdoMsAccess_CheckedChanged);
+            // 
+            // rdoSqlServer
+            // 
+            this.rdoSqlServer.AutoSize = true;
+            this.rdoSqlServer.Location = new System.Drawing.Point(123, 55);
+            this.rdoSqlServer.Name = "rdoSqlServer";
+            this.rdoSqlServer.Size = new System.Drawing.Size(74, 17);
+            this.rdoSqlServer.TabIndex = 3;
+            this.rdoSqlServer.TabStop = true;
+            this.rdoSqlServer.Text = "Sql Server";
+            this.rdoSqlServer.UseVisualStyleBackColor = true;
+            this.rdoSqlServer.CheckedChanged += new System.EventHandler(this.RdoSqlServer_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rdoSqlServer);
+            this.Controls.Add(this.rdoMsAccess);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.RadioButton rdoMsAccess;
+        private System.Windows.Forms.RadioButton rdoSqlServer;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+namespace TestSqlCommon
+{
+    public partial class Form1 : Form
+    {
+        #region Fields
+        DbConnection connection;
+        DbCommand command;
+        DataTable table = new DataTable();
+        #endregion Fields
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = table;
+        }
+
+        private void RdoMsAccess_CheckedChanged(object sender, EventArgs e)
+        {
+            String connection_string = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=.\\DB\\DbTest.accdb;";
+            String command_text = "SELECT * FROM Staffs";
+            this.connection = new OleDbConnection(connection_string);
+            this.command = this.connection.CreateCommand();
+            this.command.CommandText = command_text;
+        }
+
+        private void RdoSqlServer_CheckedChanged(object sender, EventArgs e)
+        {
+            String connection_string = "Server=.;Database=DbTest;Integrated Security=true;";
+            String command_text = "SELECT * FROM Staffs;";
+            this.connection = new SqlConnection(connection_string);
+            this.command = this.connection.CreateCommand();
+            this.command.CommandText = command_text;
+        }
+
+        private async void BtnLoadData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await connection.OpenAsync();
+                DbDataReader reader = await command.ExecuteReaderAsync();
+                table.Clear();
+                table.Load(reader);
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
